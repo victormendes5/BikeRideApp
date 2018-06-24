e2e4ce3767cb0c99814c62ccca9fa30af0bba4e4
Unknown <joaovictor_mendes@hotmail.com>
2018-06-24 17:28:52 -0300

    Refactor class name and adds menu at main activity

--- app/src/main/java/com/infnet/bikeride/bikeride/DeliverymanReviewActivity.java
+++ app/src/main/java/com/infnet/bikeride/bikeride/DeliverymanReviewActivity.java
@@ -3,11 +3,11 @@ package com.infnet.bikeride.bikeride;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Acompanhamento_Entrega extends AppCompatActivity {
+public class DeliverymanReviewActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_acompanhamento__entrega);
+        setContentView(R.layout.activity_review_deliveryman);
     }
 }
