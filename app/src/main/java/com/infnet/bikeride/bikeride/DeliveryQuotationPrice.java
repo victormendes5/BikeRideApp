4d08d26e1da8dfb1c93022a127c11f3a7a17de08
Roger Freret <roger.freret@al.infnet.edu.br>
2018-07-22 17:47:28 -0300

    Sistema de navegacao personalizado com Toolbar e Navigation Drawer (Classe BikerideContentViewBuilder)

--- app/src/main/java/com/infnet/bikeride/bikeride/DeliveryQuotationPrice.java
+++ app/src/main/java/com/infnet/bikeride/bikeride/DeliveryQuotationPrice.java
@@ -5,9 +5,22 @@ import android.os.Bundle;
 
 public class DeliveryQuotationPrice extends AppCompatActivity {
 
+    // ---> Customized setContentView with navigation drawer and toolbar
+    BikeRideContentViewBuilder mContentViewBuilder;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_delivery_quotation_price);
+
+        mContentViewBuilder = new BikeRideContentViewBuilder(this,
+                R.layout.activity_delivery_quotation_price);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (mContentViewBuilder.isNavigationDrawerClosed()) {
+            super.onBackPressed();
+        }
     }
 }
