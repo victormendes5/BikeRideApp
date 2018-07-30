76fcb876cb3c25021866d5b4f3f606e05839a555
Ronan Costa <ronan.souza@al.infnet.edu.br>
2018-07-30 16:31:15 -0300

    Logins Funcionando Finalmente

--- app/src/main/java/com/infnet/bikeride/bikeride/ConfigurationFirebase.java
+++ app/src/main/java/com/infnet/bikeride/bikeride/ConfigurationFirebase.java
@@ -1,13 +1,10 @@
 package com.infnet.bikeride.bikeride;
 
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 
 public class ConfigurationFirebase {
 
 
-    private static DatabaseReference referenceFirebase;
     private static FirebaseAuth autentication;
 
 //    public static DatabaseReference getFirebase (){
--- app/src/main/java/com/infnet/bikeride/bikeride/MainActivity.java
+++ app/src/main/java/com/infnet/bikeride/bikeride/MainActivity.java
@@ -22,34 +22,31 @@ import com.facebook.appevents.AppEventsLogger;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
-import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseError;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FacebookAuthCredential;
 import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity{
 
+    private static final String TAG = "";
+
     // ---> Customized setContentView with navigation drawer and toolbar
     BikeRideContentViewBuilder mContentViewBuilder;
-
-    private Intent intent;
 
 
     private EditText edtEmail;
@@ -75,29 +72,30 @@ public class MainActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        autentication = FirebaseAuth.getInstance();
-
+        autentication = ConfigurationFirebase.getFirebaseAuth();
 
         mContentViewBuilder = new BikeRideContentViewBuilder(this, R.layout.activity_main);
 
 
-//        user = autentication.getCurrentUser();
-//        if (user == null){
-//            setContentView(R.layout.main_drawer_layout);
+        user = autentication.getCurrentUser();
+        if (user != null){
+
+            Toast.makeText(this,"Usuário Logado",Toast.LENGTH_SHORT).show();
+
+            Redirect();
+
+        } else {
 
             //Declaração de EdiText
-
             edtEmail = (EditText) findViewById(R.id.edtLogin);
             edtPassword = (EditText) findViewById(R.id.edtPassword);
 
             //Declaração de Buttons
-
             btnLogin = (Button) findViewById(R.id.btnLogin);
             btnFacebookLogin = (LoginButton) findViewById(R.id.login_button);
             btnGooglePlus = (SignInButton) findViewById(R.id.sign_in_button);
             btnSignUp = (Button) findViewById(R.id.btnSignUp);
             btnLostLoginData = (Button) findViewById(R.id.btnLostLoginData);
-
 
 
             // Configuração Login Anonimo
@@ -118,15 +116,16 @@ public class MainActivity extends AppCompatActivity{
             //Configuração Sign Up
             btnSignUp.setOnClickListener(SignUp);
 
+            // Configure Google Sign In
+            GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                    .requestIdToken(getString(R.string.default_web_client_id))
+                    .requestEmail()
+                    .build();
 
 
+            mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
 
-//        } else {
-//
-//
-//        }
-
-
+        }
     }
 
 
@@ -166,8 +165,6 @@ public class MainActivity extends AppCompatActivity{
     // Função de Validar Login Auth Anonimo
     private void ValidarLogin(){
 
-        autentication = ConfigurationFirebase.getFirebaseAuth();
-
         autentication.signInWithEmailAndPassword(users.getEmail(), users.getPassword()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
@@ -175,9 +172,13 @@ public class MainActivity extends AppCompatActivity{
                 if (task.isSuccessful()){
 
                     Toast.makeText(MainActivity.this,"Sucesso ao Logar",Toast.LENGTH_SHORT).show();
-                }else {
 
-                    Toast.makeText(MainActivity.this,"Erro ao Logar",Toast.LENGTH_SHORT).show();
+                    Redirect();
+
+                }else if(!task.isSuccessful()) {
+                    Log.w(TAG, "signInWithEmail:failed", task.getException());
+
+                    Toast.makeText(MainActivity.this,"Erroe",Toast.LENGTH_SHORT).show();
                 }
             }
         });
@@ -185,49 +186,40 @@ public class MainActivity extends AppCompatActivity{
     }
 
     //Login Google
-
     public  View.OnClickListener LogarGoogle = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
 
-            autentication = ConfigurationFirebase.getFirebaseAuth();
+            Toast.makeText(MainActivity.this,"Clocou no sign in google" ,Toast.LENGTH_SHORT).show();
 
-            // Configure Google Sign In
-            GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                    .requestIdToken(getString(R.string.default_web_client_id))
-                    .requestEmail()
-                    .build();
-
-
-            mGoogleSignInClient = GoogleSignIn.getClient(MainActivity.this, gso);
-
-
-            signIn();
+            signInGoogle();
 
         }
     };
 
 
-    private void signIn() {
+    private void signInGoogle() {
         Intent signInIntent = mGoogleSignInClient.getSignInIntent();
-        startActivityForResult(signInIntent, 101);
+        startActivityForResult(signInIntent, 1);
+
     }
 
     private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
 
         AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
         autentication.signInWithCredential(credential)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                .addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
 
                             Toast.makeText(MainActivity.this,"login com google sucesso" ,Toast.LENGTH_SHORT).show();
 
+                            Redirect();
 
                         } else {
 
-                            Toast.makeText(MainActivity.this,"Errou Rude otario" ,Toast.LENGTH_SHORT).show();
+                            Toast.makeText(MainActivity.this, FirebaseAuthException.class.toString(),Toast.LENGTH_SHORT).show();
 
 
                         }
@@ -248,7 +240,6 @@ public class MainActivity extends AppCompatActivity{
                 handleFacebookToken(loginResult.getAccessToken());
 
                 Toast.makeText(MainActivity.this,"Entrou o login com Facebook",Toast.LENGTH_SHORT).show();
-
 
             }
 
@@ -278,10 +269,11 @@ public class MainActivity extends AppCompatActivity{
                 if(task.isSuccessful()){
 
                     Toast.makeText(MainActivity.this,"Sucesso Login Facebook no Firebase",Toast.LENGTH_SHORT).show();
-                    intent = new Intent(MainActivity.this, MainActivity.class);
-                    startActivity(intent);
+
+                    Redirect();
+
                 }else {
-                    Toast.makeText(MainActivity.this,"Error Login Facebook",Toast.LENGTH_SHORT).show();
+                    Toast.makeText(MainActivity.this, FirebaseError.class.toString(),Toast.LENGTH_SHORT).show();
                 }
             }
         });
@@ -297,16 +289,16 @@ public class MainActivity extends AppCompatActivity{
         super.onActivityResult(requestCode, resultCode, data);
 
         // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
-        if (requestCode == 101) {
+        if (requestCode == 1) {
+
             Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
             try {
-
                 // Google Sign In was successful, authenticate with Firebase
                 GoogleSignInAccount account = task.getResult(ApiException.class);
                 firebaseAuthWithGoogle(account);
             } catch (ApiException e) {
-
-
+                // Google Sign In failed, update UI appropriately
+                // ...
             }
         }
 
@@ -321,24 +313,21 @@ public class MainActivity extends AppCompatActivity{
         @Override
         public void onClick(View v) {
 
-            intent = new Intent(MainActivity.this, SingUpActivity.class);
-            startActivity(intent);
+            mContentViewBuilder.navigate(SingUpActivity.class);
 
         }
     };
 
 
-    // Logout
+    // Redirect Page
 
-    private void Logout(){
+    private void Redirect(){
 
-    autentication.signOut();
 
-//        onRestart();
-
-//        intent = new Intent(MainActivity.this, MainActivity.class);
-//        startActivity(intent);
+        mContentViewBuilder.navigate(DeliveryMainActivity.class);
 
     }
+
+
 }
 
